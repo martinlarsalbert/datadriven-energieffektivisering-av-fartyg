cbddad3d08db355d7aa22da7757ca060ebc37c2a
Martin Alexandersson <marale@kth.se>
2020-11-13 16:22:01 +0100

    time to go home.

---
+++ data/__init__.py
@@ -0,0 +1,2 @@
+import os.path
+path = os.path.dirname(__file__)
---
+++ src/data/__init__.py
@@ -0,0 +1,5 @@
+import os.path
+path = os.path.dirname(__file__)
+
+
+path_lots = r'\\sspa.local\gbg\Projekt\2020\41209668-CONNECT-(M3)\03_Project\080_Research\41209668-00\OneDrive_2020-11-13\Data Lotsbåt\rise-lots745-mars-2020-lat-lon.csv'
