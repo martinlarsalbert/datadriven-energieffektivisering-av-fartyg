e93da5648e87788633acd6c58e0226fae58ec1d7
Martin Alexandersson <martin.alexandersson@sspa.se>
2020-11-16 11:00:27 +0100

    Added the pre-commit hook that cleans the notebook output.

--- setup.py
+++ setup.py
@@ -1,4 +1,42 @@
 from setuptools import find_packages, setup
+import os.path
+import subprocess
+import sys
+from setuptools.command.install import install as _install
+
+
+def get_pwd_candidates():
+    """attempt to get a working directory for running `pre-commit install` in.
+    - pip installs in a temporary directory so `os.getcwd()` won't work
+    - `$PWD` is likely to work, but won't work on windows
+    - `$VIRTUAL_ENV` probably works, if your virtualenv is in your git repo
+    - `os.path.dirname(sys.executable)` is another potential option
+    """
+    for v in ('PWD', 'VIRTUAL_ENV'):
+        if os.environ.get(v):
+            yield os.environ[v]
+    yield os.path.dirname(sys.executable)
+
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        for pwd in get_pwd_candidates():
+            cmd = (sys.executable, '-m', 'pre_commit', 'install')
+            if not subprocess.call(cmd, cwd=pwd):
+                return
+        else:
+            raise SystemExit(r'wellp, I tried ¯\__(ツ)__/¯')
+
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+    class bdist_wheel(_bdist_wheel):
+        def run(self):
+            raise SystemExit('this is fine')
+except ImportError:
+    bdist_wheel = None
 
 setup(
     name='src',
@@ -7,4 +45,6 @@ setup(
     description='Some ML stuff for the project datadriven-energieffektivisering-av-fartyg',
     author='Martin Alexandersson',
     license='MIT',
+    install_requires=['pre-commit'],
+    cmdclass={'bdist_wheel': bdist_wheel, 'install': install},
 )
