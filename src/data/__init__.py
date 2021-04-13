a95f82608bdb2c803681d0b09178f8f1a53568d1
Martin Alexandersson <marale@kth.se>
2021-04-13 11:39:23 +0200

    played around with folium maps

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,7 +1,7 @@
 import os.path
 path = os.path.dirname(__file__)
 
+path_ships = r'\\sspa.local\gbg\Projekt\2020\41209668-CONNECT-(M3)\03_Project\080_Research\41209668-02-D2E2F\data\forsea\chalmers-forsea-data-2021-03-22'
 
-path_lots = r'\\sspa.local\gbg\Projekt\2020\41209668-CONNECT-(M3)\03_Project\080_Research\41209668-00\OneDrive_2020-11-13\Data Lotsbåt\rise-lots745-mars-2020-lat-lon.csv'
-path_ships = r'\\sspa.local\gbg\Projekt\2020\41209668-CONNECT-(M3)\03_Project\080_Research\41209668-02-D2E2F\data\forsea\forsea-rise-sspa-data'
-path_hamlet = os.path.join(path_ships, '2020-01-01-till-2021-02-24-hamlet.csv')
+path_aurora = os.path.join(path_ships, '2020-01-01-till-2021-02-28-aurora.csv')
+path_tycho = os.path.join(path_ships, '2020-01-01-till-2021-02-28-tycho-brahe.csv')
