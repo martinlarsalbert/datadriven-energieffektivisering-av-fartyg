492bc0ba7b08e5eb9db0b1f65caafe68988146c3
Martin Alexandersson <marale@kth.se>
2021-04-13 18:41:55 +0200

    paths

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,12 @@
 import os.path
-path = os.path.dirname(__file__)
+path = os.path.join(os.path.split(os.path.split(os.path.dirname(__file__))[0])[0],'data')
+
+
+external = os.path.join(path,'processed')
+interim = os.path.join(path,'interim')
+processed = os.path.join(path,'processed')
+raw = os.path.join(path,'raw')
+
 
 path_ships = r'\\sspa.local\gbg\Projekt\2020\41209668-CONNECT-(M3)\03_Project\080_Research\41209668-02-D2E2F\data\forsea\chalmers-forsea-data-2021-03-22'
 
