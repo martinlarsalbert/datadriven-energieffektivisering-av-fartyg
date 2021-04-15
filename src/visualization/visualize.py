290f8f7d696d9b3c88d9f3b6d99c2f4e58ff2355
Martin Alexandersson <marale@kth.se>
2021-04-15 16:59:34 +0200

    plotly

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,7 +1,7 @@
 import folium
 import pandas as pd
 
-def plot_map(df:pd.DataFrame, time_step='30S'):
+def plot_map(df:pd.DataFrame, time_step='30S', width=1000, height=600, zoom_start=14):
 
 
     df_ = df.resample(time_step).mean()
@@ -11,7 +11,7 @@ def plot_map(df:pd.DataFrame, time_step='30S'):
     df_out = df_.loc[mask]
     df_home = df_.loc[~mask]
 
-    my_map = folium.Map(location=(df_['latitude'].mean(),df_['longitude'].mean()), zoom_start=14)
+    my_map = folium.Map(location=(df_['latitude'].mean(),df_['longitude'].mean()), zoom_start=zoom_start)
 
     points = df_[['latitude','longitude']].to_records(index=False)
     colors = list(df_['cog'].values)
@@ -19,5 +19,7 @@ def plot_map(df:pd.DataFrame, time_step='30S'):
     line = folium.ColorLine(points, colors, colormap=colormap, opacity = 0.30, popup='out', weight=1.0)
     line.add_to(my_map)
 
+    f = folium.Figure(width=width, height=height)
+    f.add_child(my_map)
 
-    return my_map
+    return f
