c85e081fff5bc729b6e81f7e040eea6b18dbabc6
Martin Alexandersson <marale@kth.se>
2021-06-04 08:07:50 +0200

    Added distance and integrated quantities to the stats.

--- src/models/pipelines/longterm/scripts/prepdata/trip_statistics/trip_statistics.py
+++ src/models/pipelines/longterm/scripts/prepdata/trip_statistics/trip_statistics.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import argparse
 from azureml.core import Dataset, Run
+import scipy.integrate
 
 def work(dataset, path:str, sample_size=1000000):
          
@@ -30,11 +31,16 @@ def statistics(df):
     
     meta = meta=dict(df.dtypes)
     meta.pop('time')
-    meta['trip_time'] = int
+    meta['trip_time'] = float
+    
     meta['start_time'] = str
     meta['end_time'] = str
-    meta['start_index'] = int
-    meta['end_index'] = int
+    meta['E1'] = 'float64'
+    meta['E2'] = 'float64'
+    meta['E3'] = 'float64'
+    meta['E4'] = 'float64'
+    meta['E'] = 'float64'
+    meta['distance'] = 'float64'
         
     return trips.apply(func=trip_statistics, meta=meta).compute()
 
@@ -47,18 +53,44 @@ def trip_statistics(trip):
     assert (pd.TimedeltaIndex(trip['time'].diff().dropna()).total_seconds() > 0).all()  # assert that rows are ordered in time
     
     start_time = trip.iloc[0]['time']
+    end_time = trip.iloc[-1]['time']
     trip['trip_time'] = pd.TimedeltaIndex(trip['time'] - start_time).total_seconds()
-
+    
+    integrated = integrate_time(trip=trip)
+    trip.drop(columns='time', inplace=True)
     df_statistics = trip.mean()
 
-    df_statistics['start_time'] = start_time
-    df_statistics['end_time'] = trip.iloc[-1]['time']
-    df_statistics['start_index'] = trip.index[0]
-    df_statistics['end_index'] = trip.index[-1]
-    
+    df_statistics['start_time'] = str(start_time)
+    df_statistics['end_time'] = str(end_time)
+            
+
+    ## Additional Integrated features:
+    df_statistics['E1'] = integrated['P1']
+    df_statistics['E2'] = integrated['P2']
+    df_statistics['E3'] = integrated['P3']
+    df_statistics['E4'] = integrated['P4']
+    df_statistics['E'] = integrated['P']
+    #
+    df_statistics['distance'] = integrated['sog']
+
+    df_statistics['trip_time'] = trip.iloc[-1]['trip_time'] - trip.iloc[0]['trip_time'] # Total trip time makes more sense.
+    df_statistics['trip_no'] = trip.iloc[0]['trip_no']
+    df_statistics['reversing'] = trip.iloc[0]['reversing']
     df_statistics['trip_direction'] = trip.iloc[0]['trip_direction']
 
     return df_statistics
+
+def integrate_time(trip):
+    trip_ = trip.copy()
+    t = trip_['trip_time']
+    assert t.dtype == 'float64'
+    
+    X = trip_.select_dtypes(include=[np.float64]) 
+    integral_trip = scipy.integrate.simps(y=X.T,x=t)
+    
+    s = pd.Series(data=integral_trip, name='integral', index=X.columns)
+    
+    return s
 
 def load_output_as_pandas_dataframe(path:str):
     df_stat = pd.read_parquet(path)
